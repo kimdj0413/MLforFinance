029688966022ba842b6ea3e501ef16561b3ec5ee
kindj0413 <ejuniya@naver.com>
2024-07-16 09:59:54 +0900

    2

--- RandomForest.py
+++ RandomForest.py
@@ -6,24 +6,24 @@ import pandas as pd
 import numpy as np
 
 # 데이터 로드 및 전처리
-df = pd.read_csv('ready.csv')
+df = pd.read_csv('../webimagecrawling/pca.csv')
 df = df.iloc[:, 1:]
 print(df)
 
 # 데이터 분리
-X_train, X_test, y_train, y_test = train_test_split(df.drop('Column49', axis=1), df['Column49'], test_size=0.2, random_state=42)
-
+X_train, X_test, y_train, y_test = train_test_split(df.drop('label', axis=1), df['label'], test_size=0.2, random_state=42)
+"""
 # 스케일링
 scaler = StandardScaler()  # MinMaxScaler(), RobustScaler(), StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
-
+"""
 # 랜덤 포레스트 분류 모델 학습
 model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X_train_scaled, y_train)
+model.fit(X_train, y_train)
 
 # 예측
-pred = model.predict(X_test_scaled)
+pred = model.predict(X_test)
 
 # 평가
 accuracy = accuracy_score(y_test, pred)
